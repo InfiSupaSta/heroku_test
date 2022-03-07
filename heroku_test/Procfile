df49effc74cbd672b875bdff02b7809bda75af84
InfSuperStar <infiwow777@yandex.ru>
2022-03-08 00:50:30 +0700

    init commit

---
+++ heroku_test/Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn heroku_test.wsgi --log-file=-
---
+++ heroku_test/new_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from new_app.views import *
+
+urlpatterns = [
+    path('hello', hello)
+]
+#  /hello
